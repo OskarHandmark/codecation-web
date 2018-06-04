cd344aa709a454398b6c01ccf9ba4beb19877fcd
Oskar Handmark <oskarhandmark@gmail.com>
2018-06-04 15:14:45 +0200

    flights

---
+++ src/Flights.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Flights extends Component {
+    render() {
+        return (
+            <div className="flights-grid">
+                <div></div>
+                <div>
+                    <p className="section-title"> Flights: </p>
+                </div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default Flights;
