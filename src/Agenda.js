52e3792123276d227e7f8916b3d0a06b57769191
Michal Stypa <michalstypa@Michals-MacBook-Pro.local>
2018-06-04 16:00:15 +0200

    CSS refactor + agenda

---
+++ src/Agenda.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Agenda extends Component {
+    render() {
+        return (
+            <section className="agenda">
+                <div className="title">Agenda</div>
+                <div className="details">Hello pls</div>
+            </section>
+        );
+    }
+}
+
+export default Agenda;
