5d487d45e4a431bde1bb510ca1739469c1a617bb
Michal Stypa <michalstypa@Michals-MacBook-Pro.local>
2018-06-04 16:59:37 +0200

    Minor changes

--- src/Airbnb.js
+++ src/Airbnb.js
@@ -11,12 +11,15 @@ class Airbnb extends Component {
                     Airbnb
                 </div>
                 <div className="details">
-                    <p>  2 October Incheckning när som helst efter 15:00 </p>
+                    <p> 2 October Incheckning när som helst efter 15:00 </p>
                     <p>9 October Checka ut senast 12:00 </p>
                     <p> https://www.airbnb.se/rooms/351221 </p>
-
                     <p>Badalona, Catalunya 08916 </p>
-                    </div>
+
+                    <a href="https://www.youtube.com/watch?v=PW0iOW61IZQ">Video 1</a><br></br>
+                    <a href="https://www.youtube.com/watch?v=fymERNWV5UA">Video 2</a><br></br>
+                    <a href="https://www.youtube.com/watch?v=ebYpLEuoSzo">Video 3</a>
+                </div>
             </section>
         );
     }
---
+++ src/Food.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Food extends Component {
+    render() {
+        return (
+            <section className="food">
+                <div className="title">Food</div>
+                <div className="details">Cous Cous - the food so nice they named it twice</div>
+            </section>
+        );
+    }
+}
+
+export default Food;
