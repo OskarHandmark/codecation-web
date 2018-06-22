fae7ff6bacc71590b0c3a5cd4b25ddff14ec9921
Oskar Handmark <oskarhandmark@gmail.com>
2018-06-22 11:09:06 +0200

    carousel

--- src/img/accommodation/index.js
+++ src/img/accommodation/index.js
@@ -1,5 +1,5 @@
 const range = n => Array.from(Array(n).keys())
 
-const images = range(15).map(n => require(`./${n}.webp`));
+const images = range(11).map(n => require(`./${n}.jpg`));
 
 export default images;
