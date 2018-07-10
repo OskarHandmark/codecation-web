5448b3b165ce18487d72bb5538e560e31a2f61b3
Oskar Handmark <oskarhandmark@gmail.com>
2018-07-10 18:53:53 +0200

    resize images, add flight info

--- src/Accommodation.js
+++ src/Accommodation.js
@@ -9,17 +9,11 @@ class Accommodation extends Component {
             <section className="accommodation">
                 <div className="title">.Accommodation</div>
                 <div style={{ width: '75%', margin: 'auto' }}>
-                    <Carousel
-                        infiniteLoop
+                    <Carousel infiniteLoop
                     >
                         {images.map(img => <div> <img src={img} /> </div>)}
                     </Carousel>
                 </div >
-                <div className="videos">
-                    <YouTube videoId="PW0iOW61IZQ" />
-                    <YouTube videoId="fymERNWV5UA" />
-                    <YouTube videoId="ebYpLEuoSzo" />
-                </div>
             </section >
         );
     }
