79c5e5cac940fdfc93642de2c7f62472992c5c9a
Oskar Handmark <oskarhandmark@gmail.com>
2018-07-23 00:38:05 +0200

    minor css

--- src/Splash.js
+++ src/Splash.js
@@ -8,6 +8,9 @@ import barcelona from './img/barcelona.jpg';
 class Splash extends React.Component {
     render() {
         return <div className="splash" style={{ backgroundImage: `url(${barcelona})` }}>
+            <div className="splash-title">
+                <h1> CODECATION 18</h1>
+            </div>
             <div className="splash-info">
                 <div className="flex">
                     <Icon size={18} icon={calendar} />
