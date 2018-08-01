f5f065c76c4f46160963e630b5b980e7bbfd8ca2
Oskar Handmark <oskarhandmark@gmail.com>
2018-08-01 09:40:22 +0200

    responsive header text

--- src/Splash.js
+++ src/Splash.js
@@ -4,6 +4,7 @@ import { calendar } from 'react-icons-kit/fa/calendar';
 import Icon from 'react-icons-kit';
 import Countdown from './Countdown';
 import barcelona from './img/barcelona.jpg';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class Splash extends React.Component {
     render() {
@@ -11,16 +12,20 @@ class Splash extends React.Component {
             <div className="splash-title">
                 <h1> CODECATION </h1>
             </div>
-            <div className="splash-info">
-                <div className="flex">
-                    <Icon size={18} icon={calendar} />
-                    <h4> 2-9 OCTOBER 2018 </h4>
-                </div>
-                <div className="flex">
-                    <Icon size={18} icon={location} />
-                    <h4> BARCELONA, SPAIN </h4>
-                </div>
-            </div>
+            <Grid fluid>
+                <Row>
+                    <Col xs={0} sm={0} md={1} lg={2} xl={3} />
+                    <Col xs={12} sm={12} md={5} lg={4} xl={3} className="splash-info">
+                        <Icon size={18} icon={calendar} />
+                        <h4> 2-9 OCTOBER 2018 </h4>
+                    </Col>
+                    <Col xs={12} sm={12} md={5} lg={4} xl={3} className="splash-info">
+                        <Icon size={18} icon={location} />
+                        <h4> BARCELONA, SPAIN </h4>
+                    </Col>
+                    <Col xs={0} sm={0} md={1} lg={2} xl={3} />
+                </Row>
+            </Grid>
             <Countdown date="2018-10-02T08:55:00" />
         </div>
     }
