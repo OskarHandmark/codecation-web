147357f0500d37b58200a0431ae8dfe1ced2f00b
Oskar Handmark <oskarhandmark@gmail.com>
2018-08-18 22:48:46 +0200

    montserrat -> google fonts

--- src/index.js
+++ src/index.js
@@ -5,9 +5,5 @@ import { unregister } from './registerServiceWorker';
 import App from './App';
 import './index.css';
 
-import 'typeface-montserrat';
-
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
